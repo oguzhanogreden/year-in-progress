0a3ceb51ca0293e29f29cb04fdcf0f4b428b068d
Ozan Öğreden <oguzhan@oguzhanogreden.com>
2022-04-17 22:19:24 +0200

    Refactored Canvas into function component

--- src/pages/year/components/Canvas.tsx
+++ src/pages/year/components/Canvas.tsx
@@ -1,62 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { FiArrowRight } from "react-icons/fi";
 import { Client } from "reactive-beeminder-client/dist/client";
-import { progress } from "../../../utils/year-progress";
+import { progress as getProgress } from "../../../utils/year-progress";
 import GoalComponent from "./Goal";
 import "./Canvas.scss";
+import { render } from "react-dom";
 
 type CanvasProps = React.HTMLAttributes<HTMLDivElement> & {
   displayGoals: string[];
   client: Client;
 };
 
-type CanvasState = {
-  progress: number;
-  year: number;
-};
+function Canvas(props: CanvasProps) {
+  const [progress, setProgress] = useState(getProgress());
 
-class Canvas extends React.Component<CanvasProps, CanvasState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      year: 2022,
-      progress: progress(),
-    };
-  }
-
-  render() {
-    const { year, progress } = this.state;
-    const { client, displayGoals } = this.props;
-
-    return (
-      <div className={this.props.className}>
-        <div className="Header">
-          <div className="Indicator"></div>
-          <div>
-            <p>This here is now.</p>
-            <p>Progress: {progress}%</p>
-          </div>
-        </div>
-
-        <div className="canvas__goal-container">
-          {displayGoals.map(goalSlug => (
-            <GoalComponent
-              key={goalSlug}
-              className={`Goal ${false ? "invalid" : ""}`} // TODO: Replace conditional with goal.target !== null
-              name={goalSlug}
-              slug={goalSlug}
-              client={client} // TODO: Add to context?
-            ></GoalComponent>
-          ))}
-        </div>
-
-        <div className="canvas__indicator canvas__indicator--end">
-          <span className="text">This side is</span>
-          <span>2023</span>
-          <FiArrowRight className="arrow"></FiArrowRight>
+  return (
+    <div className={props.className}>
+      <div className="Header">
+        <div className="Indicator"></div>
+        <div>
+          <p>This here is now.</p>
+          <p>Progress: {progress}%</p>
         </div>
       </div>
-    );
-  }
+
+      <div className="canvas__goal-container">
+        {props.displayGoals.map(goalSlug => (
+          <GoalComponent
+            key={goalSlug}
+            className={`Goal ${false ? "invalid" : ""}`} // TODO: Replace conditional with goal.target !== null
+            name={goalSlug}
+            slug={goalSlug}
+            client={props.client} // TODO: Add to context?
+          ></GoalComponent>
+        ))}
+      </div>
+
+      <div className="canvas__indicator canvas__indicator--end">
+        <span className="text">This side is</span>
+        <span>2023</span>
+        <FiArrowRight className="arrow"></FiArrowRight>
+      </div>
+    </div>
+  );
 }
+
 export default Canvas;
